c123b968cdc0a14a41bd8116424e415e89f15334
MythicKiller-ai <truemythic2.0@gmail.com>
2025-12-19 15:09:44 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,2 @@
-const clientId = "1450801405572153446";
-const redirectUri = "https://mythickiller-ai.github.io/Ultron.dashboard.xyz/"; // replace with your GitHub Pages URL
-const scopes = ["identify", "guilds"];
 
-document.getElementById("login-btn").href =
-  `https://discord.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=${scopes.join("%20")}`;
 
